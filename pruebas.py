c8518d883156159588cb81902a51932a284cf7e7
Jojo3235 <joralmand@gmail.com>
2022-10-25 20:26:18 +0200

    creación de funciones iniciales

---
+++ pruebas.py
@@ -0,0 +1,59 @@
+from random import shuffle,choice,sample
+
+SI = ["si","s","yes","y","1"]
+
+cartas = {
+    chr(0x1f0a1): 11,
+    chr(0x1f0a2): 2,
+    chr(0x1f0a3): 3,
+    chr(0x1f0a4): 4,
+    chr(0x1f0a5): 5,
+    chr(0x1f0a6): 6,
+    chr(0x1f0a7): 7,
+    chr(0x1f0a8): 8,
+    chr(0x1f0a9): 9,
+    chr(0x1f0aa): 10,
+    chr(0x1f0ab): 10,
+    chr(0x1f0ad): 10,
+    chr(0x1f0ae): 10
+}
+
+print("Cartas: {}".format(" ".join(cartas.keys())))
+print("Valores: {}".format(list(cartas.values())))
+
+lista_cartas = list(cartas)*4
+shuffle(lista_cartas)
+print(lista_cartas)
+
+
+def obtener_carta(lista):
+    carta = choice(lista_cartas)
+    lista.append(carta)
+    return lista
+
+
+def mano():
+    mano_persona=[]
+    carta1_mano=obtener_carta(mano_persona)
+    carta2_mano=obtener_carta(carta1_mano)
+    return carta2_mano
+
+def mostrar_mano():
+    carta1=mano()
+    carta1_valor=carta1[0]
+    carta2_valor=carta1[1]
+    print("Cartas: {} {} {} {}\nTotal: {}".format(carta1_valor, cartas[carta1_valor],carta2_valor,cartas[carta2_valor],cartas[carta1_valor]+cartas[carta2_valor]))
+    return cartas[carta1_valor]+cartas[carta2_valor]
+
+mano_jugador=mostrar_mano()
+mano_banca=mostrar_mano()
+print(mano_jugador)
+
+
+"""main_banca = sample(lista_cartas, 2)
+score_banca = sum(cartas[carta] for carta in main_banca)
+print("La banca tiene: {} {}  >> su score es {}".format(main_banca[0],
+                                                          main_banca[1],
+                                                          score_banca))
+
+"""
